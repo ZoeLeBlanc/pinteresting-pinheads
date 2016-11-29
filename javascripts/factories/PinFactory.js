e71ec2a5950666e10436cccb8e484d81cb9c733e
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2016-11-28 20:34:23 -0600

    got factories set up

---
+++ javascripts/factories/PinFactory.js
@@ -0,0 +1,80 @@
+"use strict";
+app.factory("PinFactory", function($q, $http, FIREBASE_CONFIG){
+	//Firebase: get all Items
+	var getPinList = function(userId){
+		return $q((resolve, reject)=>{
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/pins.json?orderBy="uid"&equalTo="${userId}"`)
+			 .success( (response)=>{
+			 	let pins = [];
+			 	Object.keys(response).forEach((key)=>{
+			 		response[key].id = key;
+			 		pins.push(response[key]);
+			 	});
+			 	resolve(pins);
+			 })
+			 .error( (errorResponse)=>{
+			 	reject(errorResponse);
+			 });
+		});
+	};
+	//Firebase: send a new item to Firebase
+	var postNewPin = function(newPin){
+		return $q((resolve, reject)=>{
+			$http.post(`${FIREBASE_CONFIG.databaseURL}/pins.json`, JSON.stringify({
+				pinTitle: newPin.pinTitle,
+				boardid: newPin.boardid,
+				url: newPin.url,
+				uid: newPin.uid
+				})
+			)
+			 .success( (postResponse)=>{
+			 	resolve(postResponse);
+			 })
+			 .error( (errorResponse)=>{
+			 	reject(errorResponse);
+			 });
+		});
+	};
+	var deletePin = function(pinId){
+		return $q((resolve, reject)=>{
+			$http.delete(`${FIREBASE_CONFIG.databaseURL}/pins/${pinId}.json`)
+			 .success( (deleteReponse)=>{
+			 	resolve(deleteReponse);
+			 })
+			 .error( (deleteError)=>{
+			 	reject(deleteError);
+			 });
+		});
+	};
+	var getSinglePin = function(pinId){
+		return $q((resolve, reject)=>{
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/pins/${pinId}.json`)
+			 .success( (getSingleReponse)=>{
+			 	resolve(getSingleReponse);
+			 })
+			 .error( (getSingleError)=>{
+			 	reject(getSingleError);
+			 });
+		});
+	};
+	var editPin = function(editPin){
+		return $q((resolve, reject)=>{
+			$http.put(`${FIREBASE_CONFIG.databaseURL}/pins/${editPin.id}.json`, 
+				JSON.stringify({
+					pinTitle: editPin.pinTitle,
+					boardid: editPin.boardid,
+					url: editPin.url,
+					uid: editPin.uid
+				})
+			)
+			 .success( (editResponse)=>{
+			 	resolve(editResponse);
+			 })
+			 .error( (errorResponse)=>{
+			 	reject(errorResponse);
+			 });
+		});
+	};
+	return {getBoardList, postNewBoard, deleteBoard, getSingleBoard, editBoard};
+});
+
